048ecfb5bcdb14032118a24ac8e039f8155a1dc5
loucid <syd.morano@gmail.com>
2020-02-28 19:08:05 -0800

    added hw 2 stuff for syd

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("space")) {
+            anim.SetBool("isPunching", true);
+        } else {
+            anim.SetBool("isPunching", false);
+        }
+	}
+}
